273fd53bab756abf79f60819b1bbad39968186c9
Aad van Esschoten <aad@kees-tm.nl>
2019-05-08 17:09:15 +0200

    Fix problem with namespace

--- src/EventSubscriber/MyEventSubscriber.php
+++ src/EventSubscriber/MyEventSubscriber.php
@@ -4,7 +4,7 @@
  * Contains \Drupal\my_event_subscriber\EventSubscriber\MyEventSubscriber.
  */
 
-namespace Drupal\my_event_subscriber\EventSubscriber;
+namespace Drupal\kees_version_checker\EventSubscriber;
 
 use Drupal;
 use Symfony\Component\HttpKernel\KernelEvents;
